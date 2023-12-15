6d66134fc540e3933603a607469cd4337340434a
devkbm <kbm0417@nate.com>
2023-12-15 15:26:24 +0900

    2023-12-15

--- src/main/java/com/like/ConnectBoot32Application.java
+++ src/main/java/com/like/ConnectBoot32Application.java
@@ -4,28 +4,14 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import jakarta.annotation.PostConstruct;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 
 @SpringBootApplication
 public class ConnectBoot32Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConnectBoot32Application.class, args);
-	}
-
-	@PersistenceContext
-	EntityManager em;
-
-	@Bean
-	JPAQueryFactory queryFactory() {
-		return new JPAQueryFactory(em);
-		
 	}
 	
 	@PostConstruct
