1c1141c1b9b94932afe79467bdbd7de737dfaca5
devkbm <kbm0417@nate.com>
2024-05-31 17:40:41 +0900

    2024-05-31

--- src/main/java/com/like/system/login/adapter/in/web/LoginController.java
+++ src/main/java/com/like/system/login/adapter/in/web/LoginController.java
@@ -4,21 +4,50 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.like.core.web.util.WebRequestUtil;
 import com.like.system.login.application.port.in.LoginUseCase;
+import com.like.system.login.application.port.out.AuthenticationTokenSavePort;
+import com.like.system.login.application.service.LoginRequestContext;
 import com.like.system.login.dto.LoginRequestDTO;
+import com.like.system.menu.dto.MenuGroupSaveDTO;
+import com.like.system.menurole.application.port.in.external.MenuGroupByUserSelectUseCase;
+import com.like.system.permission.application.port.in.AuthenticationTokenSelectUseCase;
 import com.like.system.permission.domain.AuthenticationToken;
+import com.like.system.user.application.port.in.external.SystemUserCommonSelectUseCase;
+import com.like.system.user.domain.SystemUser;
 
 @RestController
 public class LoginController {		
 		
 	private LoginUseCase useCase;
 		 
-	public LoginController(LoginUseCase useCase) {		
+	SystemUserCommonSelectUseCase userPort;
+	MenuGroupByUserSelectUseCase menuGroupSelectUseCase;
+	AuthenticationTokenSavePort	tokenPort;
+	AuthenticationTokenSelectUseCase authTokenSelectUseCase;
+	
+	ApplicationEventPublisher publisher;
+	
+	public LoginController(LoginUseCase useCase
+						  ,SystemUserCommonSelectUseCase userPort
+						  ,MenuGroupByUserSelectUseCase menuGroupSelectUseCase
+						  ,AuthenticationTokenSavePort tokenPort
+						  ,AuthenticationTokenSelectUseCase authTokenSelectUseCase
+						  ,ApplicationEventPublisher publisher) {		
 		this.useCase = useCase;
+		
+		this.userPort = userPort;
+		this.menuGroupSelectUseCase = menuGroupSelectUseCase;		
+		this.tokenPort = tokenPort;
+		this.authTokenSelectUseCase = authTokenSelectUseCase;
+		this.publisher = publisher;
 	}
 		 
 	
@@ -28,7 +57,19 @@ public class LoginController {
 		//String ipAddress = WebRequestUtil.getIpAddress(request);
 		//System.out.println("접속 IP주소: " + ipAddress);
 		
-		return useCase.login(dto, request);
+		// 로그인 요청정보 SpringSecurityUserService에서 사용하기 위해 THREAD_LOCAL에 저장
+		LoginRequestContext.set(dto);
+		
+		SystemUser systemUser = userPort.findUser(dto.companyCode(), dto.staffNo());
+		List<MenuGroupSaveDTO> menuGroupList = menuGroupSelectUseCase.select(dto.companyCode(), dto.staffNo());
+							
+		AuthenticationToken token2 = authTokenSelectUseCase.select(dto.companyCode(), dto.staffNo(), request.getSession().getId(), WebRequestUtil.getIpAddress(request));
+		tokenPort.process(dto, systemUser.getAuthorities(), token2, request);
+		
+		LoginRequestContext.remove();
+		
+		//return useCase.login(dto, request);
+		return token2;
 	}	
 	
 }
--- src/main/java/com/like/system/login/adapter/out/web/AuthenticationTokenSaveAdapter.java
+++ src/main/java/com/like/system/login/adapter/out/web/AuthenticationTokenSaveAdapter.java
@@ -1,7 +1,6 @@
 package com.like.system.login.adapter.out.web;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,13 +10,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Component;
 
-import com.like.core.web.util.WebRequestUtil;
+
 import com.like.system.login.application.port.out.AuthenticationTokenSavePort;
 import com.like.system.login.dto.LoginRequestDTO;
-import com.like.system.menu.dto.MenuGroupSaveDTO;
 import com.like.system.permission.domain.AuthenticationToken;
-import com.like.system.user.domain.SystemUser;
-import com.like.system.user.external.SystemUserDTO;
+
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -28,26 +25,7 @@ public class AuthenticationTokenSaveAdapter implements AuthenticationTokenSavePo
 	
 	AuthenticationTokenSaveAdapter(AuthenticationManager authenticationManager) {
 		this.authenticationManager = authenticationManager;
-	}
-	
-	@Override
-	public AuthenticationToken SaveAuthenticationToken(LoginRequestDTO dto, SystemUser systemUser, List<MenuGroupSaveDTO> menuGroupList, HttpServletRequest request) {		
-								
-		AuthenticationToken authToken = createAuthToken(SystemUserDTO.toDTO(systemUser), menuGroupList, request);
-		
-		UsernamePasswordAuthenticationToken securityToken = new UsernamePasswordAuthenticationToken(dto.staffNo(), dto.password(), systemUser.getAuthorities());				
-		securityToken.setDetails(authToken);
-		
-		Authentication authentication = authenticationManager.authenticate(securityToken); 					
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		request.getSession().setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
-					
-		return authToken;
-	}
-
-	private AuthenticationToken createAuthToken(SystemUserDTO systemUser, List<MenuGroupSaveDTO> menuGroupList, HttpServletRequest request) {
-		return AuthenticationToken.of(systemUser, menuGroupList, WebRequestUtil.getIpAddress(request), request.getSession().getId());
-	}
+	}		
 	
 	public void process(LoginRequestDTO dto, Collection<? extends GrantedAuthority> getAuthorities, AuthenticationToken authToken, HttpServletRequest request) {
 		UsernamePasswordAuthenticationToken securityToken = new UsernamePasswordAuthenticationToken(dto.staffNo(), dto.password(), getAuthorities);				
